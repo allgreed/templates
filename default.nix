e48e4eb5ebcc9173a9bf16db9261398543cd4dbc
Olgierd Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2022-10-24 15:58:18 +0200

    Simplify

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
 let
-  nixpkgs = builtins.fetchGit {
+  pkgs = import (builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
     rev = "f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5"; # 27-06-2022
     # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
-  };
-  pkgs = import nixpkgs { config = {}; };
+  }) { config = {}; };
   pythonPkgs = python-packages: with python-packages; [
     ptpython # nicer repl
   ];
