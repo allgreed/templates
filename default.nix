5d37fd7a1da6b41d34f1357cef13af0a64c815f0
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2023-04-12 23:54:09 +0200

    bump nixpkgs

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
-    rev = "f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5"; # 27-06-2022
+    rev = "3fb8eedc450286d5092e4953118212fa21091b3b"; # 12-04-2022
     # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
   }) { config = {}; };
   pythonPkgs = python-packages: with python-packages; [
