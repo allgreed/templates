72997396259fb4d189a76e01281b5487e4956205
Olgierd Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2023-06-09 22:34:14 +0200

    Bump nixpkgs

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
-    rev = "3fb8eedc450286d5092e4953118212fa21091b3b"; # 12-04-2022
+    rev = "a64b73e07d4aa65cfcbda29ecf78eaf9e72e44bd"; # 9-06-2023
     # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
   }) { config = {}; };
   pythonPkgs = python-packages: with python-packages; [
