928f2b8425a8ab3c2402f5348329fadf9d87a65d
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2024-03-04 23:33:00 +0100

    babump

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
-    rev = "a64b73e07d4aa65cfcbda29ecf78eaf9e72e44bd"; # 9-06-2023
+    rev = "b8697e57f10292a6165a20f03d2f42920dfaf973"; # 4-03-2024
     # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
   }) { config = {}; };
   pythonPkgs = python-packages: with python-packages; [
@@ -11,7 +11,7 @@ let
   pythonCore = pkgs.python310;
   myPython = pythonCore.withPackages pythonPkgs;
 in
-pkgs.mkShell {
+pkgs.mkShellNoCC {
   buildInputs =
   with pkgs;
   [
