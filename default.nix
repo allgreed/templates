87879a68273882e9a227c341d01555528ea394cc
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2025-02-24 21:55:36 +0100

    Use more shallow clone!

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 let
-  pkgs = import (builtins.fetchGit {
+  pkgs = import (builtins.fetchTree {
+    type = "git";
     url = "https://github.com/nixos/nixpkgs/";
-    ref = "refs/heads/nixos-unstable";
     rev = "d74a2335ac9c133d6bbec9fc98d91a77f1604c1f"; # 17-02-2025
+    shallow = true;
     # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
   }) { config = {}; };
 
