93b52f5610d020e3bc683ea697d1f4984dd12909
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2025-03-27 20:02:37 +0100

    Add narHash

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@ let
     type = "git";
     url = "https://github.com/nixos/nixpkgs/";
     rev = "d74a2335ac9c133d6bbec9fc98d91a77f1604c1f"; # 17-02-2025
-    shallow = true;
-    # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
+    narHash = "sha256-zON2GNBkzsIyALlOCFiEBcIjI4w38GYOb+P+R4S8Jsw=";
+    # obtain via `nix-prefetch-git https://github.com/nixos/nixpkgs/ --rev $(git ls-remote https://github.com/nixos/nixpkgs nixos-unstable)`
   }) { config = {}; };
 
   # helpers
@@ -61,6 +61,8 @@ rec {
       ruff-lsp
 
       podman
+
+      nix-prefetch-git
     ];
   };
 }
